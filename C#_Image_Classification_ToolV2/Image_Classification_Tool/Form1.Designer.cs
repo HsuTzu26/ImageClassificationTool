dd240b9d09b7789192b5d5338f587cb7613e1cf8
HsuTzu26 <winstonhsu26@gmail.com>
2024-01-20 20:08:17 +0800

    feat. ImageClassificationTool
    Developed by  C# and Python
    
    by HsuTzu

---
+++ C#_Image_Classification_ToolV2/Image_Classification_Tool/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Image_Classification_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.uploadbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.compbtn = new System.Windows.Forms.Button();
+            this.irrebtn = new System.Windows.Forms.Button();
+            this.manualbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hollbtn = new System.Windows.Forms.Button();
+            this.rarebtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uploadbtn
+            // 
+            this.uploadbtn.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uploadbtn.Location = new System.Drawing.Point(1036, 12);
+            this.uploadbtn.Name = "uploadbtn";
+            this.uploadbtn.Size = new System.Drawing.Size(150, 74);
+            this.uploadbtn.TabIndex = 2;
+            this.uploadbtn.Text = "Upload Photo\r\n";
+            this.uploadbtn.UseVisualStyleBackColor = true;
+            this.uploadbtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(243, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(652, 478);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // compbtn
+            // 
+            this.compbtn.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.compbtn.Location = new System.Drawing.Point(356, 529);
+            this.compbtn.Name = "compbtn";
+            this.compbtn.Size = new System.Drawing.Size(148, 85);
+            this.compbtn.TabIndex = 4;
+            this.compbtn.Text = "Complete network";
+            this.compbtn.UseVisualStyleBackColor = true;
+            this.compbtn.Click += new System.EventHandler(this.compbtn_Click);
+            // 
+            // irrebtn
+            // 
+            this.irrebtn.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.irrebtn.Location = new System.Drawing.Point(639, 529);
+            this.irrebtn.Name = "irrebtn";
+            this.irrebtn.Size = new System.Drawing.Size(139, 85);
+            this.irrebtn.TabIndex = 5;
+            this.irrebtn.Text = "Irregular contour";
+            this.irrebtn.UseVisualStyleBackColor = true;
+            this.irrebtn.Click += new System.EventHandler(this.irrebtn_Click);
+            // 
+            // manualbtn
+            // 
+            this.manualbtn.BackColor = System.Drawing.Color.Transparent;
+            this.manualbtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("manualbtn.BackgroundImage")));
+            this.manualbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.manualbtn.FlatAppearance.BorderSize = 0;
+            this.manualbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.manualbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.manualbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.manualbtn.Location = new System.Drawing.Point(20, 10);
+            this.manualbtn.Name = "manualbtn";
+            this.manualbtn.Size = new System.Drawing.Size(50, 54);
+            this.manualbtn.TabIndex = 6;
+            this.manualbtn.UseVisualStyleBackColor = false;
+            this.manualbtn.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Manual";
+            // 
+            // hollbtn
+            // 
+            this.hollbtn.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.hollbtn.Location = new System.Drawing.Point(639, 651);
+            this.hollbtn.Name = "hollbtn";
+            this.hollbtn.Size = new System.Drawing.Size(139, 84);
+            this.hollbtn.TabIndex = 9;
+            this.hollbtn.Text = "Hollow network";
+            this.hollbtn.UseVisualStyleBackColor = true;
+            this.hollbtn.Click += new System.EventHandler(this.hollbtn_Click);
+            // 
+            // rarebtn
+            // 
+            this.rarebtn.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rarebtn.Location = new System.Drawing.Point(356, 651);
+            this.rarebtn.Name = "rarebtn";
+            this.rarebtn.Size = new System.Drawing.Size(148, 84);
+            this.rarebtn.TabIndex = 10;
+            this.rarebtn.Text = "Rare junction";
+            this.rarebtn.UseVisualStyleBackColor = true;
+            this.rarebtn.Click += new System.EventHandler(this.rarebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 755);
+            this.Controls.Add(this.rarebtn);
+            this.Controls.Add(this.hollbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.manualbtn);
+            this.Controls.Add(this.irrebtn);
+            this.Controls.Add(this.compbtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.uploadbtn);
+            this.Name = "Form1";
+            this.Text = "Image classification tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button uploadbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button compbtn;
+        private System.Windows.Forms.Button irrebtn;
+        private System.Windows.Forms.Button manualbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button hollbtn;
+        private System.Windows.Forms.Button rarebtn;
+    }
+}
+
---
+++ C#_Image_Classification_ToolV2/Image_Classification_Tool/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// Using CSV
+using CsvHelper;
+using System.Collections;
+
+// Using InputBox
+using Microsoft.VisualBasic;
+using System.Drawing.Imaging;
+
+using System.Windows.Media.Imaging;
+
+namespace Image_Classification_Tool
+{
+    public partial class Form1 : Form
+    {
+        public static int fileCount;
+        public static int index;
+        public static string dir;
+        public static string csvPath;
+
+        public static int btnCount = 0;
+
+        //csv class
+        public class CsvWrite
+        {
+            public string FileName { get; set; }
+            public string Result { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            compbtn.Enabled = false;
+            irrebtn.Enabled = false;
+            hollbtn.Enabled = false;
+            rarebtn.Enabled = false;
+
+            //manualbtn.Enabled = false;
+            //label1.Enabled = false;
+            //button1.Visible = false;
+            //pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+            //pictureBox1.Top = (this.ClientSize.Height - pictureBox1.Height) / 2;
+        }
+
+        //建立空白csv
+        private void CreatCsv()
+        {
+            string user = Interaction.InputBox("Enter your name.", "Please enter user name", "輸入名稱", -1, -1);
+            csvPath = dir + user + ".csv";
+            Console.WriteLine(csvPath);
+            // 建立空的CSV檔案
+            File.WriteAllText(csvPath, string.Empty);
+            // 欄位格式為 FileName, user
+            using (var writer = new StreamWriter(csvPath, true, Encoding.UTF8))
+            { writer.WriteLine($"FileName,{user}"); }
+        }
+
+        // 切換圖片
+        private void ChangeImage()
+        {
+            DirectoryInfo di = new DirectoryInfo(dir);
+            FileInfo[] files = di.GetFiles("*.tif"); //篩選副檔名
+            int fileCount = files.Length; //取得個數個數
+            //Console.WriteLine(fileCount); //顯示檔案個數
+
+            string[] files2 = Directory.GetFiles(dir, "*.tif"); // 讀取路徑下所有檔案名稱
+            foreach (var file in files2)
+            {
+                //Console.WriteLine(file);
+            }
+
+            string[] FilesInfo = new string[files.Length]; // 設置array大小為檔案個數
+            string[] tmp = new string[files.Length];
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                FilesInfo[i] = dir + files[i].ToString(); // 設置圖片檔路徑
+                //Console.WriteLine(FilesInfo[i]);
+                tmp[i] = dir + files[i].ToString();
+            }
+
+            if (index == files.Length - 1) // 在顯示最後一張時將button停止防止error
+            {
+                compbtn.Enabled = false;
+                irrebtn.Enabled = false;
+                hollbtn.Enabled = false;
+                rarebtn.Enabled = false;
+            }
+            else
+            {
+                index++;
+            }
+
+            pictureBox1.Image = Image.FromFile(FilesInfo[index]);         
+
+        }
+
+        // as to which folder then change image + write into csv
+        private void ToFolderCsv(string str)
+        {
+            string sourceFilePath = dir; // 檔案路徑
+            string destinationFolderPath = dir + str; // 目標資料夾路徑
+
+            // 確認目標資料夾存在，如果不存在則建立新資料夾
+            if (!Directory.Exists(destinationFolderPath))
+            {
+                Directory.CreateDirectory(destinationFolderPath);
+            }
+
+            // 取得檔案名稱並構建新路徑
+            //string fileName = Path.GetFileName(sourceFilePath);         
+            //string destinationFilePath = Path.Combine(destinationFolderPath, fileName);
+
+            string[] strFiles = new string[fileCount];
+            DirectoryInfo di = new DirectoryInfo(dir);
+            FileInfo[] files = di.GetFiles("*.tif"); //篩選副檔名       
+
+            string[] FilesInfo = new string[fileCount]; // 設置array大小為檔案個數
+
+            for (int i = 0; i < fileCount; i++)
+            {
+                FilesInfo[i] = dir + files[i].ToString(); // 設置圖片檔路徑
+                //Console.WriteLine(FilesInfo[i]);
+            }
+
+            for (int i = 0; i < fileCount; i++)
+            {
+                strFiles[i] = destinationFolderPath + files[i].ToString(); // 設置圖片檔新路徑
+                //Console.WriteLine(FilesGood[i]);
+            }
+
+            // 將檔案複製到新資料夾中
+            File.Copy(FilesInfo[index], strFiles[index]);
+            //System.IO.File.Copy(sourceFilePath, destinationFilePath);   
+            ChangeImage(); //變更顯示圖片
+
+            // 將檔名和result寫入csv
+            /*List<CsvWrite> csvWrites = new List<CsvWrite>();
+            if (btnCount < fileCount)
+            {
+                csvWrites.Add(new CsvWrite { FileName = files[btnCount].ToString(), Result = "good" });
+            }
+
+            using (var writer = new StreamWriter(csvPath, true, Encoding.UTF8))
+            {
+                foreach (var csvWrite in csvWrites)
+                {
+                    string row = $"{csvWrite.FileName},{csvWrite.Result}";
+                    writer.WriteLine(row);
+                }
+            }*/
+
+            btnCount++;
+        }
+
+        //user manual
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string message = "Manual:\n說明此影像分類工具之功能\n\nUpload Photo按鈕 - 按下後將跳出選擇資料夾畫面, 可自行匯入資料夾, 匯入後將會顯示第一張影像\nTips: 資料夾內限定影像檔要是 *.tif檔\n\nButton部分 -依據判斷, 按下後將會自動將影像分類至判斷之資料夾\nTips: 若無資料夾則會自動生成";
+            MessageBox.Show(message);
+        }
+
+        // Upload Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog(); // 顯示資料夾來提供使用者選擇
+            //Console.WriteLine(path.SelectedPath); // 存取選擇資料夾路徑
+
+            Console.WriteLine("TEST 有無抓到檔案");
+
+            dir = path.SelectedPath + "\\";
+
+            DirectoryInfo di = new DirectoryInfo(dir); // 抓資料夾path
+            FileInfo[] files = di.GetFiles("*.tif"); //篩選副檔名
+            fileCount = files.Length; //取得個數個數
+            Console.WriteLine(fileCount); //顯示檔案個數
+
+            string[] files2 = Directory.GetFiles(dir, "*.tif"); // 讀取路徑下所有檔案名稱
+            foreach (var file in files2)
+            {
+                Console.WriteLine(file);
+            }
+
+            string[] FilesInfo = new string[files.Length]; // 設置array大小為檔案個數
+            string[] tmp = new string[files.Length];
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                FilesInfo[i] = dir + files[i].ToString(); // 設置圖片檔路徑
+                //Console.WriteLine(FilesInfo[i]);
+                tmp[i] = dir + files[i].ToString(); // 暫存圖片檔路徑
+            }
+
+            //CreatCsv();
+
+            index = 0; // 預設起始值為0 (第一張影像)
+            pictureBox1.Image = Image.FromFile(FilesInfo[index]);
+
+            compbtn.Enabled = true;
+            irrebtn.Enabled = true;
+            hollbtn.Enabled = true;
+            rarebtn.Enabled = true;
+
+        }
+
+        // to Complete network folder
+        private void compbtn_Click(object sender, EventArgs e)
+        {
+            string path = "complete\\";
+            ToFolderCsv(path);
+        }
+
+
+        // to Irregular contour folder
+        private void irrebtn_Click(object sender, EventArgs e)
+        {
+            string path = "irregular\\";
+            ToFolderCsv(path);
+        }
+
+
+        // to Rare junction folder
+        private void rarebtn_Click(object sender, EventArgs e)
+        {
+            string path = "rare\\";
+            ToFolderCsv(path);
+        }
+
+        // to Hollow network folder
+        private void hollbtn_Click(object sender, EventArgs e)
+        {
+            string path = "hollow\\";
+            ToFolderCsv(path);
+        }
+
+
+
+
+    }
+}
